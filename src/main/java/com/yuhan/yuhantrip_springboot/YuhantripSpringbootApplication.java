a09e448558add0a53e2d25498f20b1f9498b1d38
zeron13570 <zeron13570@gmail.com>
2024-06-05 02:02:46 +0900

    프로젝트 생성

---
+++ src/main/java/com/yuhan/yuhantrip_springboot/YuhantripSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.yuhan.yuhantrip_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuhantripSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuhantripSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuhan/yuhantrip_springboot/YuhantripSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuhan.yuhantrip_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuhantripSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
